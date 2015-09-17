d8ecbc75b4cacd1a0edae226f6dc34976056ca5e
Hefei Li <lhfeilaile@gmail.com>
2015-09-17 20:03:55 +0800

    kt release.

--- app_jobs/app_jobs-136/myjobs/vdnlog_clean_by_ds.sh
+++ app_jobs/app_jobs-136/myjobs/vdnlog_clean_by_ds.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-cd $HADOOP_HOME
+HADOOP_HOME=/usr/hdp/2.2.4.2-2/hadoop
 
-current=$(date +%Y-%m-%d -d -1day)
+current=$(date +%Y-%m-%d -d -16day)
 
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY/ds=$current
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY_FULLY/ds=$current
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY_FULLY_GATHER/ds=$current
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY_FULLY_REPORT/ds=$current
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_LOGS/ds=$current
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_LOGS_FULLY/ds=$current
-bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/output/$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY_FULLY/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY_FULLY_GATHER/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_AVLB_MINUTELY_FULLY_REPORT/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_LOGS/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/VDN_LOGS_FULLY/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/cloudland/vdnlogs/output/$current
 
+$HADOOP_HOME/bin/hdfs dfs -rm -r /apps/hive/warehouse/vdn_avlb_minutely_report/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /apps/hive/warehouse/vdn_fluent_minutel_report/ds=$current
+$HADOOP_HOME/bin/hdfs dfs -rm -r /apps/hive/warehouse/vdn_group_by_err/ds=$current
 
-bin/hdfs dfs -rm -r /apps/hive/warehouse/vdn_avlb_minutely_report/ds=$current
-bin/hdfs dfs -rm -r /apps/hive/warehouse/vdn_fluent_minutel_report/ds=$current
-bin/hdfs dfs -rm -r /apps/hive/warehouse/vdn_group_by_err/ds=$current
+rm -rf /data/app_tmp/vdnlogs/input/$current/
